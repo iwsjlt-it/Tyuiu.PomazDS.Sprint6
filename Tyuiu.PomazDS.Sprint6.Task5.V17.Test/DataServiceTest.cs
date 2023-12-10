0aca2b3ea8ab23cc70d403a671dfa7e79f94da38
admin <zed-n.v@mail.ru>
2023-12-10 18:51:26 +0500

    task5

--- Tyuiu.PomazDS.Sprint6.Task5.V17.Test/DataServiceTest.cs
+++ Tyuiu.PomazDS.Sprint6.Task5.V17.Test/DataServiceTest.cs
@@ -11,6 +11,13 @@ namespace Tyuiu.PomazDS.Sprint6.Task5.V17.Test
         [TestMethod]
         public void TestMethod1()
         {
+            DataService ds = new DataService();
+           
+            double[] resultArray = {-17,-14.32,-7.84,-1.57,-3.64,-13.26,-8.91,-17.77,-9,-1.49,-7};
+            string path = $@"C:\Users\admin\source\repos\Tyuiu.PomazDS.Sprint6\Tyuiu.PomazDS.Sprint6.Task5.V17\bin\Debug\InPutFileTask5V17.txt";
+            double[] array = ds.LoadFromDataFile(path);
+
+            CollectionAssert.AreEqual(array, resultArray);
         }
     }
 }
